f745862563b04cdd50b203434091011d9a4e32e3
Dan Brook <dan@broquaint.com>
2020-12-03 12:38:45 +0000

    Day 1 part 2

--- src/advent_of_code_2020_clj/day1.clj
+++ src/advent_of_code_2020_clj/day1.clj
@@ -5,7 +5,7 @@
   (let [lines (line-seq (io/reader (io/resource "day1")))]
     (map #(Integer/valueOf %) lines)))
 
-(defn calc [lines]
+(defn calc-part1 [lines]
   (let [[n1 n2] (first
                  (filter (fn [[n1 n2]] (= 2020 (+ n1 n2)))
                          (for [[offset n1] (map-indexed #(list (+ %1 1) %2) lines)
@@ -14,6 +14,20 @@
                            )))]
     (* n1 n2)))
 
+(defn calc-part2 [lines]
+  (let [[n1 n2 n3] (first
+                 (filter (fn [[n1 n2 n3]] (= 2020 (+ n1 n2 n3)))
+                         (for [[offset1 n1] (map-indexed #(list (+ %1 1) %2) lines)
+                               [offset2 n2] (map-indexed #(list (+ %1 1) %2) (drop offset1 lines))
+                               n3 (drop offset2 lines)]
+                           [n1 n2 n3]
+                           )))]
+    (* n1 n2 n3)))
+
+
 (defn output []
   (calc (read-input)))
 
+(comment
+  (calc-part2 (read-input))
+  )
