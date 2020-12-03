955228d7c1506f354dd73f6acb1593ff09a4f0a8
Dan Brook <dan@broquaint.com>
2020-12-03 13:06:26 +0000

    Day 2 part 2

--- src/advent_of_code_2020_clj/day2.clj
+++ src/advent_of_code_2020_clj/day2.clj
@@ -9,7 +9,7 @@
     (let [[_ min max char password] (first (re-seq #"(\d+)-(\d+) ([a-z]): (\w+)" line))]
       [(Integer/valueOf min) (Integer/valueOf max) (first char) password])))
 
-(defn find-valid-passwords [inputs]
+(defn find-valid-passwords-p1 [inputs]
   (filter
    (fn [[min max char password]]
      (when-some [freq ((frequencies password) char)]
@@ -18,13 +18,26 @@
         (<= freq max))))
    inputs))
 
+(defn find-valid-passwords-p2 [inputs]
+  (filter
+   (fn [[pos1 pos2 char password]]
+     (let [c1 (get password (- pos1 1))
+           c2 (get password (- pos2 1))]
+       (or
+        (and (= c1 char) (not (= c2 char)))
+        (and (= c2 char) (not (= c1 char))))))
+   inputs))
+
 (defn output []
   (let [lines (read-input)
         inputs (parse-input lines)]
-    (count (find-valid-passwords inputs))))
+    (count (find-valid-passwords-p2 inputs))))
 
 (comment
-  (parse-input ["1-3 a: abcde" "1-3 b: cdefg" "2-9 c: ccccccccc"]))
+  (parse-input ["1-3 a: abcde" "1-3 b: cdefg" "2-9 c: ccccccccc"])
   (let [inputs (parse-input ["1-3 a: abcde" "1-3 b: cdefg" "2-9 c: ccccccccc"])]
-   (find-valid-passwords inputs))
-  )
+    (find-valid-passwords-p1 inputs))
+  (let [inputs (parse-input ["1-3 a: abcde" "1-3 b: cdefg" "2-9 c: ccccccccc"])]
+    (find-valid-passwords-p2 inputs))
+
+)
