88d7a0ac28278b28a6b333ac2f83c9ccd1bcbe92
Dan Brook <dan@broquaint.com>
2020-12-04 11:41:06 +0000

    Day 4 part 2

--- src/advent_of_code_2020_clj/day4.clj
+++ src/advent_of_code_2020_clj/day4.clj
@@ -23,15 +23,45 @@
   (let [passports (filter #(not (= "" (first %))) (partition-by #(= % "") lines))]
     (vec (map
           (fn [p]
-            (map #(clojure.string/split %1 #":")
-             (clojure.string/split (clojure.string/join " " p) #" ")))
+            (let [pairs (map #(clojure.string/split %1 #":")
+                             (clojure.string/split (clojure.string/join " " p) #" "))]
+              (apply hash-map (flatten pairs))))
           passports))))
 
+(defn valid-fields? [p]
+  (letfn [(matches [re s]
+            (re-matches (re-pattern (str "^" re "$")) (p s)))]
+    (some? (and
+            ;; byr (Birth Year) - four digits; at least 1920 and at most 2002.
+            (matches #"(?:19[2-9]\d)|(?:200[0-2])" "byr")
+            ;; iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+            (matches #"20(?:1\d|20)" "iyr")
+            ;; eyr (Expiration Year) - four digits ; at least 2020 and at most 2030.
+            (matches #"20(?:2\d|30)" "eyr")
+            ;; hgt (Height) - a number followed by either cm or in:
+            ;; If cm, the number must be at least 150 and at most 193.
+            ;; If in, the number must be at least 59 and at most 76.
+            (matches #"(?:1(?:[5-8]\d|9[0-3])cm)|(?:(?:59|6\d|7[0-6])in)" "hgt")
+            ;; hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+            (matches #"#[0-9a-f]{6}" "hcl")
+            ;; ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+            (#{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} (p "ecl"))
+            ;; pid (Passport ID) - a nine-digit number, including leading zeroes.
+            (matches #"\d{9}" "pid")
+            ;; cid (Country ID) - ignored, missing or not.
+            ))))
+
+(defn is-valid-passport-p1 [p]
+  (clojure.set/superset?
+   (set (keys p))
+   required-fields))
+
 (defn is-valid-passport [p]
-  (let [p-fields (set (map first p))]
+  (and
    (clojure.set/superset?
-    p-fields
-    required-fields)))
+    (set (keys p))
+    required-fields)
+   (valid-fields? p)))
 
 (defn output []
   (let [passports (parse-input (read-input))]
@@ -54,4 +84,5 @@
   (parse-input example-input)
   (map is-valid-passport (parse-input example-input))
   (is-valid-passport (first (parse-input example-input)))
+  (apply hash-map (flatten (first (parse-input example-input))))
   )
