5bbc5127a1f8c78c136505f497fa449a8957b733
Dan Brook <dan@broquaint.com>
2020-12-06 12:57:34 +0000

    Day 6 part 2

--- src/advent_of_code_2020_clj/day6.clj
+++ src/advent_of_code_2020_clj/day6.clj
@@ -4,24 +4,47 @@
 (defn read-input []
   (line-seq (io/reader (io/resource "day6"))))
 
-(defn parse-input [lines]
+(defn parse-input-p1 [lines]
   (vec
    (map (partial clojure.string/join "")
     (filter #(not (= "" (first %))) (partition-by #(= % "") lines)))))
 
-(defn count-answers [groups]
+(defn parse-input [lines]
+  (vec
+   (filter #(not (= "" (first %))) (partition-by #(= % "") lines))))
+
+(defn count-answers-p1 [groups]
   (map
    (fn [g]
      (count (set g)))
    groups))
 
-(defn output []
+(defn find-group-answers [group]
+  (let [tot (count group)
+        freqs (frequencies (clojure.string/join group))]
+    (filter (fn [[_ v]] (= tot v)) freqs)))
+
+(defn count-answers [groups]
+  (map count
+   (map find-group-answers groups)))
+
+(defn part1 []
   (let [groups (parse-input (read-input))]
-    (apply + (count-answers groups))))
+    (apply + (count-answers-p1 groups))))
+
+(defn output []
+  (let [groups (parse-input (read-input))
+        counts (count-answers groups)]
+    (apply + counts)))
 
 (comment
-  (def lines ["w" "s" "q" "s" "" "klfrwivqhc" "w" "wgyze" "anw"])
+  ;; (def lines ["w" "s" "q" "s" "" "klfrwivqhc" "w" "wgyze" "anw"])
+  (def lines ["abc" "" "a" "b" "c" "" "ab" "ac" "" "a" "a" "a" "a" "" "b"])
   (parse-input lines)
+  (filter (fn [[k v]] (= 4 v))
+          (frequencies (clojure.string/join "" (second (parse-input lines)))))
+  (find-group-answers (second (parse-input lines)))
+  (map frequencies (parse-input lines))
   (count-answers (parse-input lines))
   (filter #(not (= "" (first %))) (partition-by #(= % "") lines))
   )
