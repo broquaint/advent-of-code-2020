4e7988c7d53a71c9e264e24dac7f83e7443003dd
Dan Brook <dan@broquaint.com>
2020-12-08 23:00:53 +0000

    Day 8 part 2
    It seemed onerous at first but once I realised that the implication in
    the previous part of revisiting an instruction implying an infinite
    loop the solution was straightforward.

--- src/advent_of_code_2020_clj/day8.clj
+++ src/advent_of_code_2020_clj/day8.clj
@@ -23,15 +23,40 @@
       :acc [(+ acc val) (+ pc 1)]
       :jmp [acc (+ pc val)]))
 
-(defn run-ops [ops acc cur-pc seen]
+(defn run-ops-p1 [ops acc cur-pc seen]
   (let [[pc op val] (ops cur-pc)]
     (if (seen pc)
       acc
       (let [[new-acc new-pc] (run-op [op val] acc pc)]
         (recur ops new-acc new-pc (conj seen pc))))))
 
+(defn run-ops
+  ([ops] (run-ops ops 0 0 #{}))
+  ([ops acc cur-pc seen]
+   (if (= cur-pc (count ops))
+     acc
+     (let [[pc op val] (ops cur-pc)]
+       (if (seen pc)
+         :fail
+         (let [[new-acc new-pc] (run-op [op val] acc pc)]
+           (recur ops new-acc new-pc (conj seen pc))))))))
+
+(defn output-p1 []
+  (run-ops-p1 (parse-input (read-input)) 0 0 #{}))
+
+(def switch-op-map {:jmp :nop :nop :jmp})
+(defn switch-op [[idx op val] ops]
+  (assoc ops idx [idx (switch-op-map op) val]))
+
 (defn output []
-  (run-ops (parse-input (read-input)) 0 0 #{}))
+  (let [ops (parse-input (read-input))
+        relevant-ops (filter (fn [[_ op _]] (or (= op :jmp) (= op :nop))) ops)]
+    (loop [remaining-ops relevant-ops]
+      (let [test-ops (switch-op (first remaining-ops) ops)
+            result (run-ops test-ops)]
+        (if (= :fail result)
+          (recur (rest remaining-ops))
+          result)))))
 
 (comment
   (def lines ["nop +0"
@@ -47,4 +72,5 @@
   (parse-input lines)
   (run-op [:acc 3] 3 0)
   (run-ops (parse-input (read-input)) 0 0 #{})
+  (switch-op [3 :jmp 22] [:x :y :z :replaceme :etc])
   )
