82c6881db9d621a962b1ede8e647be1b4e38b84c
Dan Brook <dan@broquaint.com>
2020-12-09 16:44:57 +0000

    Day 9 part 2

--- src/advent_of_code_2020_clj/day9.clj
+++ src/advent_of_code_2020_clj/day9.clj
@@ -36,13 +36,36 @@
       (recur (conj (vec (rest to-consider)) n) (rest remainder) (first remainder))
       n)))
 
-(defn output []
+(defn add-up [target x remainder]
+  (loop [acc x
+         rem remainder]
+    (let [y (first rem)
+          result (+ acc y)]
+      (if (or (empty? rem) (> result target))
+        false
+        (if (= result target)
+          [x y]
+          (recur result (rest rem)))))))
+
+(defn find-contiguous-range [target nums]
+  (loop [n (first nums)
+         remainder (rest nums)]
+    (if-let [result (add-up target n remainder)]
+      (concat (take-while #(not (= % (second result))) remainder) result)
+      (recur (first remainder) (rest remainder)))))
+
+(defn output-p1 []
   (find-invalid (parse-input (read-input)) 25))
+
+(defn output []
+  (let [nums (parse-input (read-input))
+        result (find-contiguous-range 1492208709 nums)]
+    (+ (apply min result) (apply max result))))
 
 (comment
   (def lines ["35" "20" "15" "25" "47" "40" "62" "55" "65" "95" "102" "117" "150" "182" "127" "219" "299" "277" "309" "567"])
   (def nums (parse-input lines))
   (is-valid 40 (take 5 nums))
   (is-valid 127 [95 102 117 150 182])
-  
+  (find-contiguous-range 127 nums)
   )
